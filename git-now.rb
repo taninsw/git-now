bf6c0dc062bead92c2c980c42b1249aa7bd8c7ce
taninsw <tanin.sw@gmail.com>
2011-09-07 06:09:27 +0900

    change tag

--- git-now.rb
+++ git-now.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class GitNow < Formula
-  url  'https://github.com/taninsw/git-now.git', :tag => 'v0.0.3'
-  version '0.0.3'
+  url  'https://github.com/taninsw/git-now.git', :tag => 'f0.0.4'
+  version '0.0.4'
   head 'https://github.com/taninsw/git-now.git', :branch => 'develop'
   homepage 'https://github.com/taninsw/git-now'
 
